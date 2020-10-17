cd96ecda6803b5f3e2b667aafc1b76d1eaa55f6d
Max Kohler <max@koehler-kn.de>
2020-10-17 16:49:51 +0100

    Develop desin

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.scrollY > 10) {
+    document.body.classList.add("scrolled");
+  } else {
+    document.body.classList.remove("scrolled");
+  }
+  window.addEventListener("scroll", (e) => {
+    document.body.style.setProperty("--y", window.scrollY);
+    if (window.scrollY > 10) {
+      document.body.classList.add("scrolled");
+    } else {
+      document.body.classList.remove("scrolled");
+    }
+  });
+});
