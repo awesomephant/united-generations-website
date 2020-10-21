66b7de4935c551bc5714daa22bfdb9eb439557f2
Max Kohler <max@koehler-kn.de>
2020-10-21 12:19:51 +0100

    Move slider animation to javascript-land

--- js/index.js
+++ js/index.js
@@ -1,10 +1,47 @@
+let slider;
+let currentX = 0;
+let loopPoint = null;
+
+function sliderLoop() {
+  if (loopPoint !== null) {
+    if (currentX > loopPoint) {
+      currentX = loopPoint - currentX;
+    }
+  }
+  slider.style.transform = `translateX(${currentX * -1}px)`;
+  currentX += 1.8;
+  window.requestAnimationFrame(sliderLoop);
+}
+
+function setLoopPoint(lastSlide) {
+  let box = lastSlide.getBoundingClientRect();
+  loopPoint = box.x;
+}
+
 function initSliderAnimation() {
-  const slideImages = document.querySelectorAll(".slide img");
-  const slider = document.querySelector(".hero-slider");
-  slideImages[0].addEventListener("load", (e) => {
-    slider.classList.add('loaded')
+  // We start the animation as soon as possible
+  // Then we figure out the position of the last slide,
+  // which is where we need to loop back to the beginning.
+
+  slider = document.querySelector(".hero-slider");
+  let loadedSlides = 0;
+  window.setTimeout(() => {
+    slider.classList.add("loaded");
+    sliderLoop();
+  }, 1500);
+
+  const slides = document.querySelectorAll(".slide img");
+  slides.forEach((slide) => {
+    slide.addEventListener("load", (e) => {
+      loadedSlides++;
+      if (loadedSlides === slides.length) {
+        setLoopPoint(slides[slides.length - 1]);
+        window.addEventListener("resize", (e) => {
+          setLoopPoint(slides[slides.length - 1]);
+        });
+      }
+    });
   });
-  slider.style.animationDuration = `${slideImages.length * 14}s`;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
