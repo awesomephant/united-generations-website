ebf14ee74e2b710c0973cceaa0c6f987da467267
Max Kohler <max@koehler-kn.de>
2020-12-30 12:27:05 +0000

    Tweak spacing

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,6 @@ const pluginSass = require("eleventy-plugin-sass");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const markdownIt = require("markdown-it");
 const md = new markdownIt();
-const siteSettings = require("./_data/settings.json");
-const fs = require("fs");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("events", function (collectionApi) {
@@ -30,34 +28,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("renderMarkdown", function (value) {
     return md.render(value);
-  });
-  eleventyConfig.addFilter("srcset", function (url, format) {
-    const sizes = [500, 1000, 1500, 2000];
-    let file = url.split(".");
-    let output = [];
-    sizes.forEach((s) => {
-      let filename = `${file[0]}@${s}w.${format}`;
-      try {
-        fs.accessSync(`./_site/${filename}`, fs.constants.R_OK);
-        output.push(`${filename} ${s}w`);
-      } catch (err) {
-        // console.log(`Media file ${filename} does not exist, skipping.`);
-      }
-    });
-    return output.join(",");
-  });
-  eleventyConfig.addFilter("resize", function (url, size) {
-    let file = url.split(".");
-    let output = "";
-    let filename = `${file[0]}@${size}w.jpg`;
-    let path = `_site${filename}`;
-    try {
-      fs.accessSync(path, fs.constants.R_OK);
-      output = `${filename}`;
-    } catch (err) {
-      console.log(`Media file ${path} does not exist, skipping.`);
-    }
-    return output;
   });
 
   eleventyConfig.addPassthroughCopy("./assets");
