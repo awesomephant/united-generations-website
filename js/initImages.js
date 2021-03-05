1a28cc3f1dbb6e41848b121c252142cd411c3804
Max Kohler <max@koehler-kn.de>
2021-03-05 17:09:57 +0000

    Tweak image spacing

--- js/initImages.js
+++ js/initImages.js
@@ -1,5 +1,16 @@
 function initImages() {
   const images = document.querySelectorAll("img");
+  let paragraphs = document.querySelectorAll(
+    ".post--content p"
+  );
+  for (let i = 0; i < paragraphs.length; i++) {
+    let image = paragraphs[i].querySelector("img, video, iframe");
+    if (image) {
+      paragraphs[i].classList.add("hasimage");
+    }
+  }
+  
+
   images.forEach((img) => {
     if (img.complete) {
       img.classList.add("loaded");
