965684a0fd1161de77a5303cc7da12c1efdcf7af
Max Kohler <max@koehler-kn.de>
2021-03-13 09:21:08 +0000

    Restore upcoming indicator to event cards

--- js/initEvents.js
+++ js/initEvents.js
@@ -1,15 +1,16 @@
 function initEvents() {
-  const cards = document.querySelectorAll(".events .event")
+  const cards = document.querySelectorAll(".events-list .event")
   const now = new Date()
   cards.forEach(c => {
     const date = new Date(c.getAttribute("data-date"))
+    const eyebrow = c.querySelector(".card--eyebrow")
+    const span = document.createElement("span")
     if (date > now) {
-      const eyebrow = c.querySelector(".card--eyebrow")
-      const span = document.createElement("span")
       span.classList.add("upcoming")
       span.innerText = "Upcoming"
       eyebrow.insertAdjacentElement("beforeend", span)
       c.classList.add("is-upcoming")
+    } else {
     }
   })
 }
