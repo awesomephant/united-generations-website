fd0b535eead1ab6e3959ebea1690b292e75d3f28
Max Kohler <hello@maxkohler.com>
2023-02-27 16:47:18 +0100

    Closes #6

--- .eleventy.js
+++ .eleventy.js
@@ -1,95 +1,88 @@
-const pluginSass = require("eleventy-plugin-sass");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const markdownIt = require("markdown-it");
-const md = new markdownIt();
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const pluginRss = require("@11ty/eleventy-plugin-rss")
+const markdownIt = require("markdown-it")
+const md = new markdownIt()
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom
 
 module.exports = function (eleventyConfig) {
-  // The following line will ship with Eleveny 1.0
-  // eleventyConfig.addGlobalData("env", process.NODE_ENV);
+  eleventyConfig.addGlobalData("env", process.NODE_ENV)
   eleventyConfig.addCollection("events", function (collectionApi) {
-    let items = collectionApi.getFilteredByGlob(["./events/*.md"]);
+    let items = collectionApi.getFilteredByGlob(["./events/*.md"])
     let sorted = items.sort((a, b) => {
       if (a.data.date < b.data.date) {
-        return 1;
+        return 1
       } else if (a.data.date > b.data.date) {
-        return -1;
+        return -1
       }
-      return 0;
-    });
-    return sorted;
-  });
+      return 0
+    })
+    return sorted
+  })
   eleventyConfig.addCollection("news", function (collectionApi) {
-    const items = collectionApi.getFilteredByGlob(["./news/*.md"]);
+    const items = collectionApi.getFilteredByGlob(["./news/*.md"])
     let sorted = items.sort((a, b) => {
       if (a.data.date < b.data.date) {
-        return 1;
+        return 1
       } else if (a.data.date > b.data.date) {
-        return -1;
+        return -1
       }
-      return 0;
-    });
-    return sorted;
-  });
+      return 0
+    })
+    return sorted
+  })
   eleventyConfig.addCollection("research", function (collectionApi) {
-    const items = collectionApi.getFilteredByGlob(["./research/*.md"]);
+    const items = collectionApi.getFilteredByGlob(["./research/*.md"])
     let sorted = items.sort((a, b) => {
       if (a.data.order < b.data.order) {
-        return -1;
+        return -1
       } else if (a.data.order > b.data.order) {
-        return 1;
+        return 1
       }
-      return 0;
-    });
-    return sorted;
-  });
+      return 0
+    })
+    return sorted
+  })
   eleventyConfig.addCollection("books", function (collectionApi) {
-    const items = collectionApi.getFilteredByGlob(["./books/*.md"]);
+    const items = collectionApi.getFilteredByGlob(["./books/*.md"])
     let sorted = items.sort((a, b) => {
       if (a.data.date < b.data.date) {
-        return 1;
+        return 1
       } else if (a.data.date > b.data.date) {
-        return -1;
+        return -1
       }
-      return 0;
-    });
-    return sorted;
-  });
+      return 0
+    })
+    return sorted
+  })
   eleventyConfig.addCollection("pages", function (collectionApi) {
-    const items = collectionApi.getFilteredByGlob(["./books/*.md"]);
-    return items;
-  });
-  eleventyConfig.addTransform(
-    "resolveImageTitles",
-    function (content, outputPath) {
-      if (outputPath.endsWith(".html")) {
-        const dom = new JSDOM(content);
-        let transformed = "";
-        const images = dom.window.document.querySelectorAll(
-          ".post--content > p > img"
-        );
-        images.forEach(img => {
-          let src = img.getAttribute("src");
-          img.setAttribute("data-full-src", src + "?nf_resize=fit&w=2000")
-          src += "?nf_resize=fit&w=1000";
-          img.setAttribute("src", src)
-          if (img.getAttribute("title") !== "") {
-            let caption = dom.window.document.createElement("span")
-            let title = img.getAttribute("title")
-            caption.innerHTML = title;
-            caption.classList.add("caption")
-            img.insertAdjacentElement("afterend", caption)
-          }
-        })
-        transformed = dom.serialize();
-        return transformed;
-      }
-      return content;
+    const items = collectionApi.getFilteredByGlob(["./books/*.md"])
+    return items
+  })
+  eleventyConfig.addTransform("resolveImageTitles", function (content, outputPath) {
+    if (outputPath.endsWith(".html")) {
+      const dom = new JSDOM(content)
+      let transformed = ""
+      const images = dom.window.document.querySelectorAll(".post--content > p > img")
+      images.forEach((img) => {
+        let src = img.getAttribute("src")
+        img.setAttribute("data-full-src", src + "?nf_resize=fit&w=2000")
+        src += "?nf_resize=fit&w=1000"
+        img.setAttribute("src", src)
+        if (img.getAttribute("title") !== "") {
+          let caption = dom.window.document.createElement("span")
+          let title = img.getAttribute("title")
+          caption.innerHTML = title
+          caption.classList.add("caption")
+          img.insertAdjacentElement("afterend", caption)
+        }
+      })
+      transformed = dom.serialize()
+      return transformed
     }
-  );
+    return content
+  })
   eleventyConfig.addShortcode("gallery", function (data) {
-    let gallery = JSON.parse(decodeURIComponent(data));
+    let gallery = JSON.parse(decodeURIComponent(data))
     items = gallery.map((item) => {
       let caption = ""
       if (item.caption) {
@@ -98,40 +91,39 @@ module.exports = function (eleventyConfig) {
       return `<figure class="gallery--item">
       <img data-full-src="${item.image}?nf_resize=fit&w=2000" src="${item.image}?nf_resize=fit&w=1000" alt="${item.alt}">
       <figcaption>${caption}</figcaption>
-      </figure>`;
-    });
+      </figure>`
+    })
 
-    return `<div class="gallery" data-count="">${items.join("\n")}</div>`;
-  });
+    return `<div class="gallery" data-count="">${items.join("\n")}</div>`
+  })
 
   eleventyConfig.addShortcode("embed", function (data) {
-    let embed = JSON.parse(decodeURIComponent(data));
+    let embed = JSON.parse(decodeURIComponent(data))
     return `<figure class="embed">
       ${embed.code}
       <figcaption>${embed.caption}</figcaption>
-    </figure>`;
-  });
-  
+    </figure>`
+  })
+
   eleventyConfig.addFilter("renderMarkdown", function (value) {
-    return md.render(value);
-  });
+    return md.render(value)
+  })
 
   eleventyConfig.addFilter("readingTime", function (s) {
-    const words = s.split(" ");
-    const minutes = words.length / 200;
-    return minutes.toFixed(1);
-  });
+    const words = s.split(" ")
+    const minutes = words.length / 200
+    return minutes.toFixed(1)
+  })
 
-  eleventyConfig.addPassthroughCopy("./assets");
-  eleventyConfig.addPassthroughCopy("./admin");
-  eleventyConfig.addPassthroughCopy("./dist");
-  eleventyConfig.addPassthroughCopy("*.png");
-  eleventyConfig.addPassthroughCopy("/*.xml");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addWatchTarget("./dist/main.js");
-  eleventyConfig.addWatchTarget("./js/**/**.js");
-  eleventyConfig.addPassthroughCopy({ "./admin/config.yml": "config.yml" });
+  eleventyConfig.addPassthroughCopy("./assets")
+  eleventyConfig.addPassthroughCopy("./admin")
+  eleventyConfig.addPassthroughCopy("./dist")
+  eleventyConfig.addPassthroughCopy("*.png")
+  eleventyConfig.addPassthroughCopy("/*.xml")
+  eleventyConfig.addPassthroughCopy("favicon.ico")
+  eleventyConfig.addWatchTarget("./dist/main.js")
+  eleventyConfig.addWatchTarget("./js/**/**.js")
+  eleventyConfig.addPassthroughCopy({ "./admin/config.yml": "config.yml" })
 
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSass, {});
-};
+  eleventyConfig.addPlugin(pluginRss)
+}
